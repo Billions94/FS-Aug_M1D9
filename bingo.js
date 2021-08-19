dfa990e3bd1a41e84f7f8f4981a52d4968240a38
Billions94 <E.a.egbedi@gmail.com>
2021-08-19 17:38:34 +0200

    New commit

--- bingo.js
+++ bingo.js
@@ -1,18 +1,38 @@
+/*
+BINGO CHALLENGE!!!
+Create programmatically (with JS) a BINGO board with 76 cells, from 1 to 76.
+Create a button to randomize a number from 1 to 76. The same number should be highlighted on the bingo board
 
+EXTRA: 
+- Take ALWAYS a new number (eg. avoid randoming the number 10 3 times)
+- Create a USER BOARD with 24 randomized numbers that highlights as the main board does
+- Let the user choose HOW MANY user boards he's willing to play with and create them.
+*/
+ 
+ 
+ 
+ 
 function displayEntireBingo()
   // TODO: Display entire 76 cells of Bingo
-let entireBingoNode = document.getElementById("bingo-container")
+let bingoContainer = document.getElementById("bingo-container")
 
-for (let bingoNumber = 1; bingoNumber <= 76; bingoNumber++) {
-    let newBingoNumber = document.createElement("div")
-    newBingoNumber.innerText = bingoNumber
-    newBingoNumber.id = bingoNumber
 
-    entireBingoNode.appendChild(newBingoNumber)
+for (let bingoNumber = 1; bingoNumber <= 77; bingoNumber++) {
+     let newBingoNode = document.createElement("div")
+     newBingoNode.innerText = bingoNumber
+     newBingoNode.id = bingoNumber
+     
+
+     bingoContainer.appendChild(newBingoNode)
 }
 
 
+function randomNumber(){
 
+    let randomNumber = Math.floor(Math.random()*80)
+    return randomNumber
+    
+}
 
 
 
@@ -21,7 +41,4 @@ for (let bingoNumber = 1; bingoNumber <= 76; bingoNumber++) {
 
     window.onload = function() {
         displayEntireBingo()
-
-
-
-    }
+    } 
