6ca382abeabbf0566dad3dc2879a8a9970e78cb3
Billions94 <E.a.egbedi@gmail.com>
2021-08-28 16:09:59 +0200

    Rework

--- bingo.js
+++ bingo.js
@@ -2,7 +2,6 @@
 BINGO CHALLENGE!!!
 Create programmatically (with JS) a BINGO board with 76 cells, from 1 to 76.
 Create a button to randomize a number from 1 to 76. The same number should be highlighted on the bingo board
-
 EXTRA: 
 - Take ALWAYS a new number (eg. avoid randoming the number 10 3 times)
 - Create a USER BOARD with 24 randomized numbers that highlights as the main board does
@@ -13,45 +12,45 @@ EXTRA:
  
  
 function displayEntireBingo(){
-  // TODO: Display entire 76 cells of Bingo
- let bingoContainer = document.getElementById("bingo-container")
-
-
- for (let bingoNumber = 1; bingoNumber <= 76; bingoNumber++) {
-      let newBingoNode = document.createElement("div")
-      newBingoNode.innerText = bingoNumber
-      newBingoNode.id = bingoNumber
-      newBingoNode.classList.add("number")
-      bingoContainer.appendChild(newBingoNode)
-      
- }
-}
-
-function randomizeCell(){
-    let randomCell = document
-   
-}
-
-
-
-
-
-
-function randomNumber() {
-    let random = Math.floor(Math.random() *100)
-    return random
-    
-}
-
-
-function changeH1Title() {
-    let changeH1 = document.querySelector("h1")
-    changeH1.innerHTML = "This is a new Bingo Game"
-}
-
-
-
-window.onload = function() {
-    displayEntireBingo()
+    // TODO: Display entire 76 cells of Bingo
+   let bingoContainer = document.getElementById("bingo-container")
+  
+  
+   for (let bingoNumber = 1; bingoNumber <= 76; bingoNumber++) {
+        let newBingoNode = document.createElement("div")
+        newBingoNode.innerText = bingoNumber
+        newBingoNode.id = bingoNumber
+        newBingoNode.classList.add("number")
+        bingoContainer.appendChild(newBingoNode)
+        
+   }
+  }
+  
+  function randomizeCell(){
+      let randomCell = document
      
-} 
+  }
+  
+  
+  
+  
+  
+  
+  function randomNumber() {
+      let random = Math.floor(Math.random() *100)
+      return random
+      
+  }
+  
+  
+  function changeH1Title() {
+      let changeH1 = document.querySelector("h1")
+      changeH1.innerHTML = "This is a new Bingo Game"
+  }
+  
+  
+  
+  window.onload = function() {
+      displayEntireBingo()
+       
+  }  
